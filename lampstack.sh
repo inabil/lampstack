209c8ff18101fab6574ab0f40f6eee70b719b33d
inabil <npdemand@gmail.com>
2026-04-22 13:37:37 +0500

    Update lampstack.sh

--- lampstack.sh
+++ lampstack.sh
@@ -1,106 +1,175 @@
 #!/bin/bash
-#Script to Auto Install LAMP Stack
-#Author nabil@techie.com
+# LAMP Stack Installer (Native MySQL + PHP 8.x)
+# Author: nabil@techie.com
 
-E_BADARGS=65
-DATE=$(date +"%Y%m%d-%T")
-
-#OSVERSION=`cat /etc/os-release | grep -w 'ID' | sed -e 's/"//g' -e 's/ID=//';`
-OSVERSION=`grep -w 'ID' < /etc/os-release | sed -e 's/"//g' -e 's/ID=//';`
-LOGS="/tmp/lampstack.log"
+set -e
 
 DBPASS="pakistan"
-# Must be root to run
-if [[ ${UID} -ne 0 ]]; then
-	        echo "You must be root to run this script"
-		exit $E_BADARGS
+OS_ID=$(grep -w ID /etc/os-release | cut -d= -f2 | tr -d '"')
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Run as root"
+    exit 1
 fi
 
-#Starting Installation
+########################################
+# RHEL / OL / Rocky / Alma (8/9)
+########################################
+if [[ "$OS_ID" =~ ^(ol|rhel|centos|rocky|almalinux)$ ]]; then
 
-if [[ $OSVERSION == "ol" ]] ||  [[ $OSVERSION == "centos" ]] ||  [[ $OSVERSION == "redhat" ]]; then
-	echo "Installing LAMP on $OSVERSION"
-	yum update -y
-	yum install httpd httpd-tools vim net-tools mod_ssl -y
-	systemctl start httpd
-	systemctl enable httpd
-	ps aux | grep httpd
-#	systemctl status httpd;
-#	apachectl graceful
-#	apachectl -S
-	yum install php php-fpm php-mysqlnd php-opcache php-gd php-xml php-mbstring -y
-	systemctl start php-fpm
-	systemctl enable php-fpm
-	yum install mariadb-server mariadb -y
-	systemctl start mariadb
-	systemctl enable mariadb
-	mysql_secure_installation <<EOF
+    echo "Detected RHEL-based OS: $OS_ID"
 
-y
-$DBPASS
-$DBPASS
-y
-y
-y
-y
+    OS_MAJOR=$(rpm -E %{rhel})
+
+    dnf -y update
+
+    ################################
+    # Apache
+    ################################
+    dnf -y install httpd httpd-tools mod_ssl vim net-tools curl
+    systemctl enable --now httpd
+
+    ################################
+    # PHP 8.x
+    ################################
+    echo "Installing PHP 8.x..."
+
+    dnf -y module reset php
+
+    if [[ $OS_MAJOR -eq 8 ]]; then
+        dnf -y module enable php:8.1
+    elif [[ $OS_MAJOR -eq 9 ]]; then
+        dnf -y module enable php:8.2
+    fi
+
+    dnf -y install \
+        php php-fpm php-mysqlnd php-opcache php-gd php-xml php-mbstring php-cli php-curl
+
+    systemctl enable --now php-fpm
+
+    ################################
+    # MySQL (Native Repo)
+    ################################
+    echo "Installing MySQL..."
+
+    dnf -y install mysql-server
+    systemctl enable --now mysqld
+
+    ################################
+    # Secure MySQL
+    ################################
+    MYSQL_TEMP_PASS=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}' || true)
+
+    if [[ -n "$MYSQL_TEMP_PASS" ]]; then
+mysql --connect-expired-password -uroot -p"$MYSQL_TEMP_PASS" <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DBPASS';
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
 EOF
-	#Apache configs
-	echo 'ServerName 127.0.0.1' >> /etc/httpd/conf/httpd.conf
-	mkdir /etc/httpd/vhosts.d
-	cp mydomain.conf /etc/httpd/vhosts.d/
-	cp ssl-mydomain.conf-default /etc/httpd/vhosts.d/
-	mkdir -p /var/www/vhosts/mydomain
-	echo '127.0.0.1 mydomain.com' >> /etc/hosts
-	touch /var/www/vhosts/mydomain/index.php
-	echo '<?php phpinfo() ?>' >> /var/www/vhosts/mydomain/index.php
-	echo 'Include vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
-	apachectl configtest
-	#Check if Apache is OK then reload
-	if apachectl -S; then
-	  apachectl graceful
-	  curl --silent http://mydomain.com/index.php | grep -w 'head'
-	else
-  	echo 'apache config failed, check errors'
-	fi
-	echo 'Database Password:'
-	echo $DBPASS
-	#mysql -uroot -p
-	#Enable PHP/HTTPD Modules if required.
-	#a2enmod rewrite
-	#phpenmod mcrypt
-	
-	#Enable port on firewall
-	#firewall-cmd --permanent --zone=public --add-service=http
-	#firewall-cmd --permanent --zone=public --add-service=https
-	#systemctl reload firewalld
-elif [[ $OSVERSION == "debian" ]] ||  [[ $OSVERSION == "ubuntu" ]] ||  [[ $OSVERSION == "pop" ]]; then
-	echo "Section in progress"
+    else
+mysql -uroot <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DBPASS';
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
+EOF
+    fi
 
-elif [[ $OSVERSION == "unknown" ]]
-then
-	echo "OS Unknown! ----- $OSVERSION "
+    ################################
+    # Apache configs (your block)
+    ################################
+    echo "Configuring Apache virtual host..."
+
+    grep -q "ServerName 127.0.0.1" /etc/httpd/conf/httpd.conf || \
+        echo 'ServerName 127.0.0.1' >> /etc/httpd/conf/httpd.conf
+
+    mkdir -p /etc/httpd/vhosts.d
+
+    # Copy only if files exist
+    [[ -f mydomain.conf ]] && cp mydomain.conf /etc/httpd/vhosts.d/
+    [[ -f ssl-mydomain.conf-default ]] && cp ssl-mydomain.conf-default /etc/httpd/vhosts.d/
+
+    mkdir -p /var/www/vhosts/mydomain
+
+    grep -q "mydomain.com" /etc/hosts || \
+        echo '127.0.0.1 mydomain.com' >> /etc/hosts
+
+    cat > /var/www/vhosts/mydomain/index.php <<EOF
+<?php phpinfo(); ?>
+EOF
+
+    grep -q "vhosts.d" /etc/httpd/conf/httpd.conf || \
+        echo 'Include vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
+
+    apachectl configtest
+
+    if apachectl -S; then
+        apachectl graceful
+        curl --silent http://mydomain.com/index.php | grep -w 'head' || true
+    else
+        echo 'apache config failed, check errors'
+    fi
+
+########################################
+# Debian / Ubuntu / Pop!_OS
+########################################
+elif [[ "$OS_ID" =~ ^(debian|ubuntu|pop)$ ]]; then
+
+    echo "Detected Debian-based OS: $OS_ID"
+
+    apt update -y
+
+    ################################
+    # Apache
+    ################################
+    apt install -y apache2 curl vim net-tools
+    systemctl enable --now apache2
+
+    ################################
+    # PHP 8.x
+    ################################
+    apt install -y \
+        php php-fpm php-mysql php-cli php-gd php-xml php-mbstring php-curl
+
+    ################################
+    # MySQL
+    ################################
+    DEBIAN_FRONTEND=noninteractive apt install -y mysql-server
+    systemctl enable --now mysql
+
+    mysql -uroot <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DBPASS';
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
+EOF
+
+    ################################
+    # Apache config
+    ################################
+    mkdir -p /var/www/html/mydomain
+
+    cat > /var/www/html/mydomain/index.php <<EOF
+<?php phpinfo(); ?>
+EOF
+
+    grep -q "mydomain.com" /etc/hosts || \
+        echo "127.0.0.1 mydomain.com" >> /etc/hosts
+
+    systemctl restart apache2
 
 else
-	echo "OS does not match required configurations!"
+    echo "Unsupported OS: $OS_ID"
+    exit 1
 fi
 
-
-
-
-
-
-
-
-
-#if [[ $CHECK == "no" ]] || [[ -z "$CHECK" ]];
-#then
-#	echo -n "OK: No SSH ROOT access enabled, "
-#else
-#	echo -n "CRITICAL: SSH ROOT access is enabled, "
-#fi
-#
-#
-#if [ ! -f $LINUXHOSTFILE ]; then
-#	echo "No $LINUXHOSTFILE found!!";
-#	exit 1;
-#fi
+########################################
+# Final Output
+########################################
+echo "===================================="
+echo "LAMP Installation Completed"
+echo "PHP Version: $(php -v | head -n 1)"
+echo "MySQL Root Password: $DBPASS"
+echo "URL: http://mydomain.com"
+echo "===================================="
